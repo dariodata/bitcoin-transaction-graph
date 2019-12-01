1f16b4a0d43dd48175b2773278c57767ece5b2d1
Dario Arcos-Diaz <dariodata@users.noreply.github.com>
2019-12-01 12:46:26 +0100

    formatting

--- gcn_model.py
+++ gcn_model.py
@@ -267,6 +267,6 @@ if __name__ == "__main__":
         model, features, labels, test_indices
     )
     print(
-        f"Test Accuracy {acc:.4f} | Precision {precision:.4f} | Recall {recall:.4f} | "\
+        f"Test Accuracy {acc:.4f} | Precision {precision:.4f} | Recall {recall:.4f} | "
         f"F1 score {f1_score:.4f}"
     )
